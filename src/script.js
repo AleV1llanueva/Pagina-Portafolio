9f559f4dea8df5f34b070d5ba712d8437ce2707a
AleV1llanueva <163602565+AleV1llanueva@users.noreply.github.com>
2024-04-19 08:50:54 -0600

    Version 1.0.0

---
+++ src/script.js
@@ -0,0 +1,26 @@
+let calculadora = document.querySelector('#calc-proy');
+let clima = document.querySelector('#clima-proy');
+let copiaNike = document.querySelector('#copia-proy');
+let instagram = document.querySelector('#instagram');
+let github = document.querySelector('#github');
+let correo = document.querySelector('#correo';)
+
+calculadora.addEventListener('click', (e)=>{
+    window.open('https://alev1llanueva.github.io/Calculadora-vs1/', '_blank')
+})
+
+clima.addEventListener('click', (e)=>{
+    window.open('https://alev1llanueva.github.io/App-de-Clima/', '_blank')
+})
+
+copiaNike.addEventListener('click', (e)=>{
+    window.open('https://alev1llanueva.github.io/replica-pagina-nike/', '_blank')
+})
+
+instagram.addEventListener('click', (e)=>{
+    window.open('https://www.instagram.com/ale_v1ll4/', '_blank')
+})
+
+github.addEventListener('click', (e)=>{
+    window.open('https://github.com/AleV1llanueva', '_blank')
+})
